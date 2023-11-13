38fc7ab24adec4abde81d74896725d887fd4c4e8
Meena P N <100338505+Meenakeechu@users.noreply.github.com>
2023-11-13 14:41:14 +0530

    Update urls.py

--- age_project/urls.py
+++ age_project/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('age_app.urls')),
+    path('AgePredictionView/', include('age_app.urls')),
 ]
 
 if settings.DEBUG:
